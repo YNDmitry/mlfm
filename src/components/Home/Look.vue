07ce77a4f57d5e28e58f33cbe7e76922f1e99021
YNDmitry <mailmetelev@gmail.com>
2024-06-03 18:20:04 +0500

    look fix

--- src/components/Home/Look.vue
+++ src/components/Home/Look.vue
@@ -20,7 +20,7 @@
 
 <template>
 	<section class="bg-red pb-16 pt-10 max-tablet:pb-9 max-tablet:pt-9">
-		<div class="mx-auto my-0 max-w-[1095px] px-3">
+		<div class="mx-auto my-0 max-w-[1095px] px-3 max-tablet:px-1">
 			<h2
 				class="text-center text-h2 font-bold uppercase text-third max-tablet:text-h2Mob max-mobile:tracking-[2.5px] mobile:tracking-[5.25px]"
 			>
@@ -52,7 +52,7 @@
 							disableOnInteraction: true,
 						}"
 						:loop="true"
-						class="relative w-full max-w-[350px] max-tablet:w-auto max-tablet:max-w-[150px]"
+						class="relative w-full max-w-[350px] max-tablet:w-auto max-tablet:max-w-[120px]"
 						id="look-swiper"
 					>
 						<SwiperSlide
@@ -77,6 +77,12 @@
 	#look-swiper .swiper-pagination {
 		position: static;
 		margin-top: 2rem;
+	}
+
+	@media screen and (max-width: 768px) {
+		#look-swiper .swiper-pagination {
+			@apply mt-4;
+		}
 	}
 
 	#look-swiper .swiper-pagination-bullet {
