230e916b195af40b6f815f7215c04b1c5e785bdc
YNDmitry <mailmetelev@gmail.com>
2024-06-06 11:03:19 +0500

    logo fix

--- src/components/Header/Logo.vue
+++ src/components/Header/Logo.vue
@@ -1,7 +1,7 @@
 <template>
 	<NuxtLink to="/" class="absolute z-50 mx-auto text-center">
 		<div>
-			<div class="text-[1.25rem] text-red max-tablet:text-[1rem]">
+			<div class="text-[1.25rem] text-red2 max-tablet:text-[1rem]">
 				MLFM.STORE
 			</div>
 			<div class="text-[0.5rem] font-light max-tablet:text-[0.3rem]">
